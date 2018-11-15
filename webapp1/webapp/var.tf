a3fa917b4f38587a2ddf5d9896724cfd8d5339c4
Ubuntu <azureops@table5dwdevopsvm.12jmcbx3mbbebku1a4yy2vgqid.nx.internal.cloudapp.net>
2018-11-15 20:36:20 +0000

    update1

---
+++ webapp1/webapp/var.tf
@@ -0,0 +1,14 @@
+variable "yo" {
+  type="string"
+  default="qewqwe"
+  description="Yo Val:"
+}
+
+variable "sub_id"{}
+variable "appid"{}
+variable "password"{}
+variable "tenant"{}
+variable "location"{}
+variable "rg"{}
+variable "sec_group_name"{}
+variable "prefix"{}
